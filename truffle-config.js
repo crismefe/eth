03a919f4bdb00e5d91aa233eafc7f76d2d70779b
crismefe <crismefe@gmail.com>
2020-02-19 23:24:36 -0400

    ultimo

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = 'goat swear swim shallow tennis grass sleep elevator cycle color sing inflict';
+
 module.exports = {
   networks: {
     development: {      
@@ -5,6 +9,14 @@ module.exports = {
       port: 7545,
       network_id: '*',
       gas: 5000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/ce94760c7e284aa1bb8523592ce1a78f"),
+      network_id: 4
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/ce94760c7e284aa1bb8523592ce1a78f"),
+      network_id: 3
     }
   }
 }
